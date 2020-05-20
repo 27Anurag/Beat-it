86838eccb2aa4ca40a1a413f999ca588b070b458
27Anurag <56363215+27Anurag@users.noreply.github.com>
2020-05-21 01:09:27 +0530

    new

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+var song;
+var slider;
+var button;
+
+
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth/2, windowHeight/2);
+	song = loadSound("songs/Avicii_Wake_Me_Up_Lyric_Video_.mp3",loaded);
+	slider = createSlider(0, 1,0.5,0.01);
+	button = createButton("Play");
+	button.mousePressed(togglePlaying)
+
+}
+
+function loaded(){
+	// song.play()
+}
+
+function togglePlaying(){
+	if (!song.isPlaying()){
+		song.play();
+		button.html("Pause");
+	}
+	else{
+		song.pause()
+		button.html("Play");
+	}
 }
 
 function draw() {
+	background(0);
+	song.setVolume(slider.value());
 
 }
